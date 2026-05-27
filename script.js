02cfb5afeb8d84fce2ecb959bdb84e51ef2ec830
ayomidebless <blessingay5@gmail.com>
2026-05-27 11:15:13 +0100

    worked on how to make the calculator work

--- script.js
+++ script.js
@@ -36,15 +36,15 @@ const displayElement = document.querySelector(".result-display")
 function checkDigitClick(digit) {
     //work on the first number
   if(operator === null) {
-  if(firstNumber === "0" || firstNumber === "") {
-    firstNumber = digit
-  } else {
-    firstNumber += digit;
-  }
-  displayValue = firstNumber    
+    if(firstNumber === "0" || firstNumber === "") {
+        firstNumber = digit
+    }else {
+        firstNumber += digit;
+    }
+    displayValue = firstNumber    
   }
     //work on the seeond number
-   else
+   else {
     if(anotherNumber === "0" || anotherNumber === "") {
         anotherNumber = digit;
     } else {
@@ -53,5 +53,52 @@ function checkDigitClick(digit) {
     displayValue = anotherNumber
 }
 updateDisplay();
+}
+// Function to show the current value to the screen
+function updateDisplay() {
+  displayElement.textContent = displayValue;
+};
+
+const digitButtons = document.querySelectorAll('.number');
+    digitButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    const digitValue = button.textContent.trim();
+    checkDigitClick(digitValue);
+    updateDisplay()
+  });
+});
+//handle when an operator button is clicked
+function checkOperatorClick(selectedOperator) {
+    if(firstNumber !== "") {
+        operator = selectedOperator;
+    }
+};
 
 
+//store the first and second numbers input by the user
+//then operate() on them when the user presses the = button
+function handleEqualTo() {
+    if(firstNumber === "" || anotherNumber === "" || !operator) {
+        return;
+    }
+        const result = operate(operator, Number(firstNumber), Number(anotherNumber));
+    displayValue = result;
+    updateDisplay()
+    //reseting the variables for the next calculation
+    firstNumber = result.toString();
+    anotherNumber = "";
+    operator = null;
+}
+//add eventlisteniner to the operator button
+const operatorButtons = document.querySelectorAll(".operator")
+    operatorButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const selectedOperator = button.textContent.trim()
+            checkOperatorClick(selectedOperator)
+        });
+    });
+    //also to th equal to
+const equalsButton = document.querySelector('.equal-to');
+equalsButton.addEventListener('click', () => {
+    handleEqualTo();
+});
